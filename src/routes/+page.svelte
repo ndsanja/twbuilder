d1a64840687ea56ccbfc4fb8ab91d5093cfac47e
ndsanja <ndsanja@gmail.com>
2022-08-24 20:02:29 +0700

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let css1 = '';
+	let css2 = '';
+</script>
+
+<input type="text" class="w-full border border-slate-500 p-2" bind:value={css1} />
+<input type="text" class="w-full border border-slate-500 p-2" bind:value={css2} />
+<h1 class={`${css1}`}>Taillwind css</h1>
+<button class={`btn btn-primary ${css2}`}>button</button>

62b469df0438d79e2b12afdb210c7eab858ba300
ndsanja <ndsanja@gmail.com>
2022-08-25 21:24:25 +0700

    add and create gsap action

--- src/lib/elements/blocks/Div.svelte
+++ src/lib/elements/blocks/Div.svelte
@@ -8,45 +8,47 @@
 	export let data: ElementTypes | undefined;
 </script>
 
-<div
-	use:tweenMe
-	on:mouseenter={() => ($elementOnHover = data?.id)}
-	on:mouseleave={() => ($elementOnHover = '')}
-	class={`${data?.styles?.components} ${data?.styles?.layout} ${data?.styles?.spacing} ${
-		data?.styles?.size
-	} ${data?.styles?.position} ${data?.styles?.typography} ${data?.styles?.backgrounds} ${
-		data?.styles?.borders
-	} ${data?.styles?.effects} ${
-		$elementOnHover === data?.id && 'relative hover:ring hover:ring-green-500 hover:ring-offset-1'
-	} ${
-		$elementIdStore === data?.id &&
-		'ring ring-red-500 ring-offset-1 hover:ring hover:ring-red-500 hover:ring-offset-1'
-	}`}
->
-	<div class="btn">{data?.id}</div>
-	<slot><!-- optional fallback --></slot>
-
+{#if window !== undefined}
 	<div
-		on:click={() => {
-			$elementIdStore = data?.id;
-			$toogleAside = true;
-		}}
-		class={`absolute -top-[15px] left-[50%] -translate-x-[50%] h-[30px] w-[30px] bg-green-500 rounded-md ring ring-green-500 ring-offset-2 ${
-			$elementOnHover === data?.id ? 'flex' : 'hidden'
-		}
-		`}
+		use:tweenMe
+		on:mouseenter={() => ($elementOnHover = data?.id)}
+		on:mouseleave={() => ($elementOnHover = '')}
+		class={`${data?.styles?.components} ${data?.styles?.layout} ${data?.styles?.spacing} ${
+			data?.styles?.size
+		} ${data?.styles?.position} ${data?.styles?.typography} ${data?.styles?.backgrounds} ${
+			data?.styles?.borders
+		} ${data?.styles?.effects} ${
+			$elementOnHover === data?.id && 'relative hover:ring hover:ring-green-500 hover:ring-offset-1'
+		} ${
+			$elementIdStore === data?.id &&
+			'ring ring-red-500 ring-offset-1 hover:ring hover:ring-red-500 hover:ring-offset-1'
+		}`}
 	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-5 w-5"
-			viewBox="0 0 20 20"
-			fill="currentColor "
+		<div class="btn">{data?.id}</div>
+		<slot><!-- optional fallback --></slot>
+
+		<div
+			on:click={() => {
+				$elementIdStore = data?.id;
+				$toogleAside = true;
+			}}
+			class={`absolute -top-[15px] left-[50%] -translate-x-[50%] h-[30px] w-[30px] bg-green-500 rounded-md ring ring-green-500 ring-offset-2 ${
+				$elementOnHover === data?.id ? 'flex' : 'hidden'
+			}
+		`}
 		>
-			<path
-				fill-rule="evenodd"
-				d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-				clip-rule="evenodd"
-			/>
-		</svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				viewBox="0 0 20 20"
+				fill="currentColor "
+			>
+				<path
+					fill-rule="evenodd"
+					d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</div>
 	</div>
-</div>
+{/if}
